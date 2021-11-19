714bbb0d4f973b97b2a51b2d60781ded6e1acd0b
tom perkins <t.perk@live.com.au>
2021-11-19 20:44:11 +1100

    created person schema for the database

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 mongoose.connect(
-  process.env.MONGO_URI, 
+  process.env['MONGO_URI'], 
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
